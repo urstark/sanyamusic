10b8549258cf8694bb592a7cea6ac3b948c3ce0a
Stark <96779742+urstark@users.noreply.github.com>
2026-01-04 12:40:07 +0530

    ғɪxᴇᴅ ʙʏ sᴛᴀʀᴋ

--- config.py
+++ config.py
@@ -39,9 +39,8 @@ GIT_TOKEN = getenv("GIT_TOKEN", None)
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/SanyaxBots")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/SanyaxSupport")
 
-# YT-API Get from @AmShashank on tg
-YOUR_API_KEY = getenv("YOUR_API_KEY", "stranger")
-YOUR_API_URL = getenv("YOUR_API_URL", "http://46.38.237.240:8000")
+# YT-API
+YT_API_TOKEN = getenv("YT_API_TOKEN")
 
 # Assistant settings
 AUTO_LEAVING_ASSISTANT = getenv("AUTO_LEAVING_ASSISTANT", "True")
@@ -110,6 +109,7 @@ if SUPPORT_CHANNEL and not re.match(r"(?:http|https)://", SUPPORT_CHANNEL):
 
 if SUPPORT_CHAT and not re.match(r"(?:http|https)://", SUPPORT_CHAT):
     raise SystemExit("[ERROR] - Your SUPPORT_CHAT url is invalid. It must start with https://")
+
 
 
 
