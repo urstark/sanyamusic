df01a18c3e20cfd099e6f9483366e7f802d02055
Stark <96779742+urstark@users.noreply.github.com>
2026-01-14 22:31:28 +0530

    ғɪxᴇᴅ ʙʏ sᴛᴀʀᴋ

--- SANYAMUSIC/plugins/bot/repo.py
+++ SANYAMUSIC/plugins/bot/repo.py
@@ -6,12 +6,12 @@ from SANYAMUSIC import app
 from config import BOT_USERNAME
 
 # Caption Text
-start_txt = """<b>✨ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <u>sᴛᴀʀᴋ ʀᴇᴘᴏs</u></b>
+start_txt = """<b>❍ ᴡᴇʟᴄᴏᴍᴇ ᴛᴏ <u>sᴛᴀʀᴋ ʀᴇᴘᴏs</u></b>
 
-🚀 <b>ᴇᴀsʏ ᴅᴇᴘʟᴏʏ</b> –ᴏɴᴇ ᴄʟɪᴄᴋ ʜᴇʀᴏᴋᴜ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ  
-🛡️ <b>ɴᴏ ʜᴇʀᴏᴋᴜ ᴏʀ ɪᴅ ʙᴀɴ ɪssᴜᴇs</b>  
-🔋 <b>ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs</b> – ʀᴜɴ 24/7 ʟᴀɢɢ-ғʀᴇᴇ  
-⚙️ <b>ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴀʟ & ᴇʀʀᴏʀ-ғʀᴇᴇ</b>  
+:⧽ <b>ᴇᴀsʏ ᴅᴇᴘʟᴏʏ</b> –ᴏɴᴇ ᴄʟɪᴄᴋ ʜᴇʀᴏᴋᴜ ᴅᴇᴘʟᴏʏᴍᴇɴᴛ  
+:⧽ <b>ɴᴏ ʜᴇʀᴏᴋᴜ ᴏʀ ɪᴅ ʙᴀɴ ɪssᴜᴇs</b>  
+:⧽ <b>ᴜɴʟɪᴍɪᴛᴇᴅ ᴅʏɴᴏs</b> – ʀᴜɴ 24/7 ʟᴀɢɢ-ғʀᴇᴇ  
+:⧽ <b>ғᴜʟʟʏ ғᴜɴᴄᴛɪᴏɴᴀʟ & ᴇʀʀᴏʀ-ғʀᴇᴇ</b>  
 
 <i>ɴᴇᴇᴅ ʜᴇʟᴘ? sᴇɴᴅ sᴄʀᴇᴇɴsʜᴏᴛ ᴛᴏ ᴛʜᴇ sᴜᴘᴘᴏʀᴛ ɢʀᴏᴜᴘ!</i>"""
 
@@ -21,17 +21,17 @@ async def repo_handler(_, msg):
     buttons = [
         [InlineKeyboardButton("➕ ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
         [
-            InlineKeyboardButton("💬 sᴜᴘᴘᴏʀᴛ", url="https://t.me/SanyaXsupport"),
-            InlineKeyboardButton("👤 ᴏᴡɴᴇʀ", url="https://t.me/cuxrs"),
+            InlineKeyboardButton("sᴜᴘᴘᴏʀᴛ", url="https://t.me/SanyaXsupport"),
+            InlineKeyboardButton("ᴏᴡɴᴇʀ", url="https://t.me/urstarkz"),
         ],
-        [InlineKeyboardButton("🧾 ᴜᴘᴅᴀᴛᴇs", url="https://t.me/sanyaxbots")],
+        [InlineKeyboardButton("ᴜᴘᴅᴀᴛᴇs", url="https://t.me/sanyaxbots")],
         [
-            InlineKeyboardButton("✨ sᴀɴʏᴀ ᴍᴜsɪᴄ", url="https://github.com/urstark/sanyamusic/fork")
+            InlineKeyboardButton("sᴀɴʏᴀ ᴍᴜsɪᴄ", url="https://github.com/urstark/sanyamusic/fork")
          ]
     ]
 
     await msg.reply_photo(
-        photo="https://files.catbox.moe/d3trfw.jpg",
+        photo="https://graph.org/file/6603c3740378d3f7187da.jpg",
         caption=start_txt,
         reply_markup=InlineKeyboardMarkup(buttons)
     )
@@ -54,12 +54,13 @@ async def repo(_, message):
             list_of_users += f"{count}. [{user['login']}]({user['html_url']})\n"
             count += 1
 
-        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/urSTARK/Sanyamusic) | [UPDATES](https://t.me/cuxrs)
+        text = f"""[𝖱𝖤𝖯𝖮 𝖫𝖨𝖭𝖪](https://github.com/urSTARK/Sanyamusic) | [UPDATES](https://t.me/urstarkz)
 | 𝖢𝖮𝖭𝖳𝖱𝖨𝖡𝖴𝖳𝖮𝖱𝖲 |
 ----------------
 {list_of_users}"""
         await app.send_message(message.chat.id, text=text, disable_web_page_preview=True)
     else:
         await app.send_message(message.chat.id, text="Failed to fetch contributors.")
+
 
 
