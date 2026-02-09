35bca45b34f38b6fd6fd400d366c2c0385b3bc9a
Stark <96779742+urstark@users.noreply.github.com>
2026-02-09 16:04:01 +0530

    - 𝑆𝑎𝑛𝑦𝑎 ♪

--- SANYAMUSIC/utils/inline/queue.py
+++ SANYAMUSIC/utils/inline/queue.py
@@ -81,16 +81,10 @@ def aq_markup(_, chat_id):
             InlineKeyboardButton(text=_["S_B_3"], url=f"https://t.me/{app.username}?startgroup=true",)
         ],
         [
-            InlineKeyboardButton(text="• ʀᴇsᴜᴍᴇ •", callback_data=f"ADMIN Resume|{chat_id}"),
-            InlineKeyboardButton(text="• ᴘᴀᴜsᴇ •", callback_data=f"ADMIN Pause|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(text="• sᴋɪᴘ •", callback_data=f"ADMIN Skip|{chat_id}"),
-            InlineKeyboardButton(text="• sᴛᴏᴘ •", callback_data=f"ADMIN Stop|{chat_id}"),
-        ],
-        [
-            InlineKeyboardButton(text="◁ 10s", callback_data=f"ADMIN SeekBack|{chat_id}"),
-            InlineKeyboardButton(text="10s ▷", callback_data=f"ADMIN Seek|{chat_id}"),
+            InlineKeyboardButton(text=" ▷ ", callback_data=f"ADMIN Resume|{chat_id}"),
+            InlineKeyboardButton(text=" II ", callback_data=f"ADMIN Pause|{chat_id}"),
+            InlineKeyboardButton(text=" ‣‣I ", callback_data=f"ADMIN Skip|{chat_id}"),
+            InlineKeyboardButton(text=" ▢ ", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
         [
             InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")
